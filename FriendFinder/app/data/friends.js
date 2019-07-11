28baa142bfda36be35be52a057c1cf163addcef4
Robert Ramirez <metaknight757@gmail.com>
2019-07-10 23:49:41 -0500

    FriendFinder

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,30 @@
+var friends = [
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+         
+    },
+    {
+        "name":"Freddy",
+        "photo":"https://www.writeups.org/wp-content/uploads/Lion-O-Thundercats-a.jpg",
+        "scores":[4, 5, 3, 2, 1, 5, 3, 2, 4, 1]
+    },
+    {
+        "name":"Mesa",
+        "photo":"http://www.thundercatsfans.org/tcshow/gallery/albums/tcho1/normal_THUNDERCATS_S2_VOL1_D1-62.jpg",
+        "scores":[1, 2, 3, 4, 5, 3, 2, 3, 2, 1]
+     },
+     {
+        "name":"Andrew",
+        "photo":"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/65f159cb-2cc5-4354-a1d3-443e73ace36e/dck8biq-7786edd8-6158-4e65-96da-abf84b62b8da.jpg/v1/fill/w_1192,h_670,q_70,strp/_wip__thundercats___panthro_by_jkorso_dck8biq-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9OTAwIiwicGF0aCI6IlwvZlwvNjVmMTU5Y2ItMmNjNS00MzU0LWExZDMtNDQzZTczYWNlMzZlXC9kY2s4YmlxLTc3ODZlZGQ4LTYxNTgtNGU2NS05NmRhLWFiZjg0YjYyYjhkYS5qcGciLCJ3aWR0aCI6Ijw9MTYwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.O3ZnBPgJBFlQzjKhGX87hj4vur8NPOnblAhZTbMctqs",
+        "scores":[5, 4, 4, 4, 5, 3, 3, 5, 4, 3]
+     },
+     {
+        "name":"Liz",
+        "photo":"https://i2.wp.com/metro.co.uk/wp-content/uploads/2011/04/article-1302000983635-0b7d271c00000578-387324_636x346.jpg?quality=90&strip=all&zoom=1&resize=540%2C293",
+        "scores":[1, 1, 3, 2, 3, 1, 2, 3, 2, 1]
+     }   
+];
+
+module.exports = friends;
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # homework9
+
+This assignment is for making a friend survey similar to a dating website or a social network site.
+
+# Features
+
+This homework shows that you can using the front end and the back end of web development. Along with making a much better survey than before.
+
+# Installation
+
+The process env I used was in PORT 3777. The main installtion is "npm install". This also includes body-parser, and the main installation of express.
+
+# How to use
+
+To start the pages, type in the command "node server" on the terminal. Make sure you are also in the FriendFinder folder when you open the terminal. The terminal show be giving you a message "App listeing on PORT 3777", then open the port on your web broswer.
+
+You will see the first page and in the page you will see the button "Go TO Survey", click on the button to go to the survey page.
+
+The second page will have 12 items, the first two items are to fill out your name then add a link for a photo of you. The next 10 items are the survey questions you are given. Answer all 10 questions by clicking the submit button to find out which of the friends you are most compatable with. It will show you the person's name and a photo of that person. Below the submit button is the button that will take you to the pervios page
+
+Below each page is a link to my github account for the code I used for this assignment, and the api friends list that I created.
+
