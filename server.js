f1247d3298cd7011dfa1be26dfe87f5e68df9c72
Robert Ramirez <metaknight757@gmail.com>
2019-08-13 19:14:32 -0500

    Completed changes

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/app/public')));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
